537792b56ac0a4691667af3f0d54818b4d99a5a0
Marc-Alexandre Espiaut <76531574+malespiaut@users.noreply.github.com>
2023-07-19 20:52:28 +0200

    [develop.h]: Remove NOMEMCHECK code.

--- rott/develop.h
+++ rott/develop.h
@@ -20,7 +20,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef _develop_public
 #define _develop_public
 
-#define NOMEMCHECK
 #define WEAPONCHEAT  1
 #define MEMORYCORRUPTIONTEST  0
 #define SYNCCHECK    1
