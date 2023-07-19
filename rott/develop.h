49f6d47de1f394bcf178006acea8bc28a9ef4d65
Marc-Alexandre Espiaut <76531574+malespiaut@users.noreply.github.com>
2023-07-19 20:51:21 +0200

    [develop.h]: Remove TEAMTEST code.

--- rott/develop.h
+++ rott/develop.h
@@ -21,7 +21,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define _develop_public
 
 #define NOMEMCHECK
-#define TEAMTEST     0
 #define LOADSAVETEST 0
 #define WEAPONCHEAT  1
 #define MEMORYCORRUPTIONTEST  0
