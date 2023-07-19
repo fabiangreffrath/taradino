70ec9fea8291eadf04eb07de43e588f447955c1e
Fabian Greffrath <fabian@greffrath.com>
2023-07-19 08:54:03 +0200

    remove lowmemory and DoPanicMapping() stuff

--- rott/rt_main.c
+++ rott/rt_main.c
@@ -1149,7 +1149,6 @@ void GameLoop (void)
                   if (
                       (!LastScan) &&
                       (!IN_GetMouseButtons()) &&
-                      (lowmemory==0) &&
                       (GameLevels.avail==false)
                      )
                      {
