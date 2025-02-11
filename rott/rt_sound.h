3d081a26e39b8456820951f44b8a93b90d7e970a
Fabian Greffrath <fabian@greffrath.com>
2025-02-11 13:28:02 +0100

    some more clean-up

--- rott/rt_sound.h
+++ rott/rt_sound.h
@@ -21,6 +21,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define _rt_sound_public
 
 #include "develop.h"
+#include "rt_def.h"
 #include "music.h"
 
 typedef enum
